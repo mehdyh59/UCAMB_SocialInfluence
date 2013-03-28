19359bcf8de077a225db3a2cf3d8c11d6d5ef520
mehdyh59 <mehdy.h59@gmail.com>
2013-03-28 16:33:42 +0000

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -1,10 +1,15 @@
 import twitter
 
 def get_authentication(screen_name=None,param=1):
-   
+    if screen_name=="X" and param==1:
+        con_key='X'
+        con_sec='X'
+        acc_tok='X'
+        acc_sec='X'
     
     api=twitter.Api(consumer_key=con_key,consumer_secret=con_sec,access_token_key=acc_tok,access_token_secret=acc_sec)
     return api
+
 
 
     
